0a9cbc5e19e8a18ec682e607a5760f0924dbf3b5
Clayton <clayton.fortini@gmail.com>
2019-11-07 21:03:55 -0500

    Adding files

---
+++ index.js
@@ -0,0 +1,26 @@
+let isPalindrome = function(word){
+    word = word.toLowerCase();
+    let len = Math.floor(word.length / 2);
+    for(let i = 0; i < len; i++){
+        if (word[i] !== word[word.length - i - 1]){
+            let ul = document.getElementById('not-palindromes');
+            let li = document.createElement('li');
+            let item = document.getElementById('testword');
+            li.appendChild(document.createTextNode(item.value));
+            ul.appendChild(li);
+            // document.getElementById('not-palindromes').appendChild(li);
+            return false;
+        }
+        else {
+            let ul = document.getElementById('palindrome-list');
+            let li = document.createElement('li');
+            let item = document.getElementById('testword');
+            li.appendChild(document.createTextNode(item.value));
+            ul.appendChild(li);
+            return true;
+        }
+    }
+}
+
+// console.log(isPalindrome('racecar'));
+// console.log(isPalindrome('doghouse'));
